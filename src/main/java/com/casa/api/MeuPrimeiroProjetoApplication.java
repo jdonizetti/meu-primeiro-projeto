a218e7fa049975af75a91ee267e9201b512514d8
Jackson <jdonizetti.90@gmail.com>
2018-05-23 21:18:02 -0300

    commit inicial

---
+++ src/main/java/com/casa/api/MeuPrimeiroProjetoApplication.java
@@ -0,0 +1,12 @@
+package com.casa.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPrimeiroProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
+	}
+}
